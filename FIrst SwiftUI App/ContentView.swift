80e2ebd5d0f19b14c81264939716ccb023ae3f33
Prabhakar Patil <prabhakar@squats.in>
2020-05-16 12:07:49 +0530

    First Commit for the Swift UI Learning path

---
+++ FIrst SwiftUI App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FIrst SwiftUI App
+//
+//  Created by Prabhakar Patil on 16/05/20.
+//  Copyright © 2020 prabhakar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
