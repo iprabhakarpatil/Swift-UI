fdf2b6d40ae509cbab3f57f8d79f1b6fa385116b
Prabhakar Patil <prabhakar@squats.in>
2020-05-24 01:13:43 +0530

    Getting Staeted with SwiftUI

--- FIrst SwiftUI App/ContentView.swift
+++ FIrst SwiftUI App/ContentView.swift
@@ -9,8 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    @State var whoIsThereVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                Text("Welcome to my first SwiftUI App")
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+                
+                Button(action: {
+                    self.alertIsVisible = true
+                    print("Hit me pressed")
+                }) {
+                    Text("Hit Me")
+                }
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    return Alert(title: Text("Hello There!!!"), message: Text("This is my first Swift UI Pop-up"), dismissButton: .default(Text("Awesome!!!")))
+                }
+                
+            }
+            Button(action: {
+                self.whoIsThereVisible = true
+                print("Knock knock tapped")
+            }) {
+                Text("Knock Knock")
+            }
+            .alert(isPresented: $whoIsThereVisible) { () -> Alert in
+                return Alert(title: Text("Knock Knock"), message: Text("Pizza delivery here"), dismissButton: .default(Text("OK")))
+            }
+        }
     }
 }
 
