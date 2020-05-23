96a495076b7898568138d36b990a8f4d273343bb
Prabhakar Patil <prabhakar@squats.in>
2020-05-24 01:54:39 +0530

    Leaning the spacer and the padding components

--- FIrst SwiftUI App/ContentView.swift
+++ FIrst SwiftUI App/ContentView.swift
@@ -15,24 +15,73 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            VStack {
-                Text("Welcome to my first SwiftUI App")
-                    .fontWeight(.bold)
-                    .foregroundColor(.green)
-                
+            
+            Spacer()
+            
+            // Target Row
+            HStack {
+                Text("Put the bullseye as close as you can to: ")
+                Text("100")
+            }
+            
+            Spacer()
+            
+            // Slider Row
+            HStack{
+                Text("1")
+                Slider(value: .constant(10))
+                Text("100")
+            }
+            
+            Spacer()
+            
+            // Button Row
+            Button(action: {
+                self.alertIsVisible = true
+                print("Hit me pressed")
+            }) {
+                Text("Hit Me")
+            }
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                return Alert(title: Text("Hello There!!!"), message: Text("This is my first Swift UI Pop-up"), dismissButton: .default(Text("Awesome!!!")))
+            }
+            
+            Spacer()
+            
+            // Score Row
+            HStack {
+                // Start Over Button
                 Button(action: {
-                    self.alertIsVisible = true
-                    print("Hit me pressed")
+                    print("Start Over button tapped")
                 }) {
-                    Text("Hit Me")
+                    Text("Start Over")
                 }
-                .alert(isPresented: $alertIsVisible) { () -> Alert in
-                    return Alert(title: Text("Hello There!!!"), message: Text("This is my first Swift UI Pop-up"), dismissButton: .default(Text("Awesome!!!")))
+                
+                Spacer()
+                
+                // Score Label:
+                Text("Score")
+                Text("9999")
+                
+                Spacer()
+                
+                // Round Label:
+                Text("Round")
+                Text("9999")
+                
+                Spacer()
+                
+                // Info Button
+                Button(action: {
+                    print("Info button tapped")
+                }) {
+                    Text("Info")
                 }
                 
             }
-
+            .padding(.bottom, 20.0)
         }
+        
     }
 }
 
